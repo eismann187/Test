4b1bddec640ba08c01e6e65f692ac86cdde724d1
eismann187 <44958200+eismann187@users.noreply.github.com>
2018-12-18 22:05:12 +0500

    Update CalculatorAppKuzminTest2.java

--- RGR/CalculatorAppKuzminTest2.java
+++ RGR/CalculatorAppKuzminTest2.java
@@ -9,11 +9,11 @@ public class CalculatorAppKuzminTest2 {
 	@Test
 	public void test() throws InterruptedException {
 		Calculator c = new Calculator(); //create instance of the calculator
-		c.textField1.setText("16"); //set test in the text field
-		c.textField2.setText("17"); //set test in the text field
-		c.textField3.setText("18"); //set test in the text field
+		c.textField1.setText("5"); //set test in the text field
+		c.textField2.setText("5"); //set test in the text field
+		c.textField3.setText("1"); //set test in the text field
 		c.countB.doClick(); //do click on the button
-		assertEquals("1.", c.textField4.getText()); //check results
+		assertEquals("2.", c.textField4.getText()); //check results
 		
 	}
 
